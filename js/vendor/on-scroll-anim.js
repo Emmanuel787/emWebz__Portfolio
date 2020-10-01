bf7166cb7895d7d1d17673f9abe6eb83d05fc09b
Emmanuel787 <ifgodwithme7@gmail.com>
2020-10-01 05:25:38 -0400

    add photo compression

--- js/vendor/on-scroll-anim.js
+++ js/vendor/on-scroll-anim.js
@@ -1,20 +1,23 @@
+// var tl = new TimelineLite();
+
+
 function onScrollAnim() {
 
   var docScroll = $(document).scrollTop(),
 
-  heroSection = $(".el-hero-section").offset().top + 400;
+  heroSection = $(".el-hero-section").offset().top + 200;
 
 
   if (docScroll > heroSection) {
-    $(".el-main-header").css({
-      "background": "#2a2a2aa6"
-    });
-  } else {
-    $(".el-main-header").css({
-      "background": ""
-    });
+    TweenMax.to(".eb__b1", 1, { y: 0, opacity: 1});
+    TweenMax.to(".eb__b2", 2, { y: 0, opacity: 1});
+    TweenMax.to(".eb__b3", 4, { y: 0, opacity: 1});
+    TweenMax.to(".eb__b4", 6, { y: 0, opacity: 1});
+  } else{
+    // TweenMax.to(".eb__b1", 1, { y: 70, opacity: 0});
+    // TweenMax.to(".eb__b2", 2, { y: 70, opacity: 0});
+    // TweenMax.to(".eb__b3", 3, { y: 70, opacity: 0});
+    // TweenMax.to(".eb__b4", 4, { y: 70, opacity: 0});
   }
-
 }
 window.addEventListener("scroll", onScrollAnim);
-
